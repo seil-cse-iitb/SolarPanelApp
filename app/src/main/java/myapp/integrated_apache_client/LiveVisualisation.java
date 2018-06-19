4ed49eee490c4b1b821cbbd49942710e600193ee
Sapan <sapantanted99@gmail.com>
2018-06-19 18:30:06 +0530

    all esp's graph is merged...only text vs graph line color is an issue which is not being same

--- app/src/main/java/myapp/integrated_apache_client/LiveVisualisation.java
+++ app/src/main/java/myapp/integrated_apache_client/LiveVisualisation.java
@@ -1,11 +1,14 @@
 package myapp.integrated_apache_client;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -25,7 +28,10 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Random;
 import java.util.Set;
 
 import model.ESPData;
@@ -33,12 +39,36 @@ import model.ESPData;
 public class LiveVisualisation extends BaseActivity {
     ListView lvEsp;
     BaseAdapter ba;
+    GraphView graph;
+    Spinner spField;
+    HashMap<String, LineGraphSeries<DataPoint>> seriesMap = new HashMap<>();
+    HashMap<String, Thread> liveDataThreads = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_live_visualisation);
         super.onCreate(savedInstanceState);
+        graph = (GraphView) findViewById(R.id.graph);
         lvEsp = (ListView) findViewById(R.id.lvEsp);
+        spField = (Spinner) findViewById(R.id.spField);
+        spField.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                for (ESP esp : espConnected) {
+                    seriesMap.get(esp.getMacAddress()).resetData(esp.getDataPointArray(spField.getSelectedItem() + ""));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        Viewport viewport = graph.getViewport();
+        viewport.setXAxisBoundsManual(true);
+        viewport.setMinX(0);
+        viewport.setMaxX(30);
+        viewport.setScalable(true);
+        graph.getGridLabelRenderer().setLabelVerticalWidth(100);
         ba = new BaseAdapter() {
             @Override
             public int getCount() {
@@ -59,104 +89,68 @@ public class LiveVisualisation extends BaseActivity {
             public View getView(final int position, View convertView, ViewGroup parent) {
                 final ESP esp = getItem(position);
                 convertView = getLayoutInflater().inflate(R.layout.my_live_list_item, null);
-                ((TextView) convertView.findViewById(R.id.name)).setText(esp.getName());
-                final GraphView graph = (GraphView) convertView.findViewById(R.id.graph);
-                final LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
-                Viewport viewport = graph.getViewport();
-                viewport.setXAxisBoundsManual(true);
-                viewport.setMinX(0);
-                viewport.setMaxX(30);
-                viewport.setScalable(true);
-                graph.getGridLabelRenderer().setLabelVerticalWidth(100);
-                graph.addSeries(series);
-                final Spinner spinner = (Spinner) convertView.findViewById(R.id.spinner);
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
 
-                Set<String> keySet = ESPData.fieldIndexMap.keySet();
-                Iterator<String> iterator = keySet.iterator();
-                final String[] array = new String[keySet.size()];
-                for (int i = 0; iterator.hasNext(); i++) {
-                    array[i] = iterator.next();
-                }
-                BaseAdapter spinnerAdapter = new BaseAdapter() {
-                    @Override
-                    public int getCount() {
-                        return array.length;
-                    }
+                CheckBox cbESPName = (CheckBox) convertView.findViewById(R.id.cbESPName);
+                cbESPName.setText(esp.getName());
 
-                    @Override
-                    public String getItem(int position) {
-                        return array[position];
-                    }
+                LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
+                series.setColor(color);
+                cbESPName.setTextColor(color);
 
+                seriesMap.put(esp.getMacAddress(), series);
+                cbESPName.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                     @Override
-                    public long getItemId(int position) {
-                        return position;
-                    }
-
-                    @Override
-                    public View getView(int position, View convertView, ViewGroup parent) {
-                        convertView = getLayoutInflater().inflate(R.layout.my_spinner_item,null);
-                        TextView tvEspName= (TextView) convertView.findViewById(R.id.tvEspName);
-                        tvEspName.setText(getItem(position));
-                        return convertView;
-                    }
-                };
-                spinner.setAdapter(spinnerAdapter);
-                final Thread liveDataThread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        HttpClient httpclient = new DefaultHttpClient();
-                        HttpGet httpGet = new HttpGet("http://" + esp.getIpAddress() + "/liveData");
-                        try {
-                            HttpResponse response = httpclient.execute(httpGet);
-                            HttpEntity entity = response.getEntity();
-                            if (entity != null) {
-                                InputStream in = entity.getContent();
-                                BufferedReader br = new BufferedReader(new InputStreamReader(in));
-                                makeToast("Live streaming starts! Name:" + esp.getName());
-                                while (true) {
-                                    try {
-                                        String line = br.readLine();
-//                                        makeToast(line);
-                                        ESPData espData = ESPData.buildESPData(line);
-                                        esp.addDataPoint(espData);
-                                        series.appendData(new DataPoint(series.getHighestValueX()+1, espData.getData(spinner.getSelectedItem() + "")), true, 300);
-                                    } catch (Exception e) {
-                                        e.printStackTrace();
-                                        break;
-                                    }
-                                }
-                            } else {
-                                makeToast("Live streaming failed!! Name:" + esp.getName());
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            makeToast("Live Stream failed!! Name:" + esp.getName(), Toast.LENGTH_LONG);
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (isChecked) {
+                            graph.addSeries(seriesMap.get(esp.getMacAddress()));
+                        } else {
+                            graph.removeSeries(seriesMap.get(esp.getMacAddress()));
                         }
                     }
                 });
-                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                        graph.setVisibility(View.VISIBLE);
-                        series.resetData(esp.getDataPointArray(spinner.getSelectedItem() + ""));
-                    }
+                if (liveDataThreads.get(esp.getMacAddress()) == null) {
+                    Thread liveDataThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            HttpClient httpclient = new DefaultHttpClient();
+                            HttpGet httpGet = new HttpGet("http://" + esp.getIpAddress() + "/liveData");
+                            try {
+                                HttpResponse response = httpclient.execute(httpGet);
+                                HttpEntity entity = response.getEntity();
+                                if (entity != null) {
+                                    InputStream in = entity.getContent();
+                                    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                                    makeToast("Live streaming starts! Name:" + esp.getName());
+                                    while (true) {
+                                        try {
+                                            String line = br.readLine();
+//                                            makeToast(line);
+                                            ESPData espData = ESPData.buildESPData(line);
+                                            esp.addDataPoint(espData);
+                                            LineGraphSeries<DataPoint> series = seriesMap.get(esp.getMacAddress());
+                                            series.appendData(new DataPoint(series.getHighestValueX() + 1, espData.getData(spField.getSelectedItem() + "")), true, 300);
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                            break;
+                                        }
+                                    }
+                                } else {
+                                    makeToast("Live streaming failed!! Name:" + esp.getName());
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                makeToast("Live Stream failed!! Name:" + esp.getName(), Toast.LENGTH_LONG);
+                            }
+                        }
+                    });
+                    liveDataThreads.put(esp.getMacAddress(), liveDataThread);
+                }
+                if (!liveDataThreads.get(esp.getMacAddress()).isAlive()) {
+                    liveDataThreads.get(esp.getMacAddress()).start();
+                }
 
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parent) {
-//                        graph.setVisibility(View.GONE);
-//                        if (liveDataThread.isAlive())
-//                            liveDataThread.interrupt();
-//                        series.resetData(new DataPoint[]{});
-                    }
-                });
-             convertView.findViewById(R.id.ibGraph).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (!liveDataThread.isAlive())
-                            liveDataThread.start();
-                    }
-                });
                 System.out.println("Showing ESP: " + esp);
                 return convertView;
             }
